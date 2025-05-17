a05c910ba18cc1a226084b9279d9a8d5fd5e3547
Leon Kim <leonmkim@seas.upenn.edu>
2025-05-17 01:01:28 -0400

    add better handling in config of different normalization strategies

--- lerobot/common/policies/diffusion/configuration_diffusion.py
+++ lerobot/common/policies/diffusion/configuration_diffusion.py
@@ -85,13 +85,25 @@ class Unet1dEncoderConfig:
 @dataclass
 class ActionConfig:
     horizon_length: int = 36
-    action_frame_expression: str = 'relative' # absolute, relative, or delta # https://umi-gripper.github.io/umi.pdf for details
+    action_frame_expression: str = 'delta' # absolute, relative, or delta # https://umi-gripper.github.io/umi.pdf for details
     rotation_representation: str = 'axis_angle' # quaternion, axis_angle, 6d
+    maniskill_prenormalized: bool = True # whether the action is normalized to [-1, 1] range
+    output_maniskill_normalized: bool = True # whether the action is normalized to [-1, 1] range
+    pos_upper: float = 0.1 # upper bound for position action
+    pos_lower: float = -0.1 # lower bound for position action
+    rot_lower: float = -0.1 # lower bound for rotation action
+    rot_upper: float = 0.1 # upper bound for rotation action
 
     def __post_init__(self):
         assert self.horizon_length > 0, "Action horizon must be greater than 0"
         assert self.action_frame_expression in ['absolute', 'relative', 'delta'], "Action frame expression must be one of ['absolute', 'relative', 'delta']"
         assert self.rotation_representation in ['axis_angle', 'quaternion', '6d'], "Rotation representation must be one of ['axis_angle', 'quaternion', '6d']"
+
+        if self.action_frame_expression != 'delta':
+            # assert not self.maniskill_prenormalized, "Maniskill prenormalization is only supported for delta action frame expression"
+            print("Warning: Maniskill prenormalization is not supported for absolute or relative action frame expression. Setting maniskill_prenormalized to False.")
+            self.maniskill_prenormalized = False
+            self.output_maniskill_normalized = False
 
         self.action_dim = 3
         if self.rotation_representation == 'axis_angle':
@@ -106,9 +118,14 @@ class ActionConfig:
 class ActionHistoryConfig:
     enable: bool = False
     history_length: int = 8
-    action_frame_expression: str = 'relative' # absolute, relative, or delta # https://umi-gripper.github.io/umi.pdf for details
+    action_frame_expression: str = 'delta' # absolute, relative, or delta # https://umi-gripper.github.io/umi.pdf for details
     action_frame: str = 'current_end_effector' # previous_end_effector, current_end_effector # only relevant if using relative frame
     rotation_representation: str = 'axis_angle' # quaternion, axis_angle, 6d
+    maniskill_prenormalized: bool = True # whether the action is normalized to [-1, 1] range
+    pos_upper: float = 0.1 # upper bound for position action
+    pos_lower: float = -0.1 # lower bound for position action
+    rot_lower: float = -0.1 # lower bound for rotation action
+    rot_upper: float = 0.1 # upper bound for rotation action
 
     def __post_init__(self):
         if self.enable:
@@ -116,6 +133,11 @@ class ActionHistoryConfig:
         assert self.action_frame_expression in ['absolute', 'relative', 'delta'], "Action frame expression must be one of ['absolute', 'relative', 'delta']"
         assert self.rotation_representation in ['axis_angle', 'quaternion', '6d'], "Rotation representation must be one of ['axis_angle', 'quaternion', '6d']"
         assert self.action_frame in ['previous_end_effector', 'current_end_effector'], "Action frame must be one of ['previous_end_effector', 'current_end_effector']"
+
+        if self.action_frame_expression != 'delta':
+            # assert not self.maniskill_prenormalized, "Maniskill prenormalization is only supported for delta action frame expression"
+            print("Warning: Maniskill prenormalization is not supported for absolute or relative action frame expression. Setting maniskill_prenormalized to False.")
+            self.maniskill_prenormalized = False
 
         self.action_dim = 3
         if self.rotation_representation == 'axis_angle':
